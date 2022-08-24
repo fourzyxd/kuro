a5dc0649b013382f4089e81e19addf4fb9db6d45
Fourzylol <anakthanos222@gmail.com>
2022-08-24 11:01:41 +0700

    anjay

---
+++ plugins/info-sc.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://instagram.com/yudtya.w*\nFollow Untuk Mengetahui Update Selanjutnya:https://github.com/fourzylol\nSilahkan follow github saya ,Terimakasih`
+conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
+}
+handler.command = handler.help = ['sc']
+handler.tags = ['info']
+
+export default handler
